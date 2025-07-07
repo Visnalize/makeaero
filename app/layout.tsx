2ef3359d7c9993592003388f9c8b684771e4c37d
Khang <khang.nguyenduyvn@gmail.com>
2025-07-08 00:53:50 +0700

    fix: seo metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,20 @@
-import './globals.css'
+import { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Make Aero - Frutiger Aero Style Generator",
+  description:
+    "Your go-to tool for creating authentic Frutiger Aero styles with ease, fully customizable and filled with nostalgia.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
